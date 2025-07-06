fedebf87be7624626edb02776dbdbc77fe812db5
yaman <yamansm12@gmail.com>
2025-07-06 19:14:19 +0300

    added apis foundation

---
+++ piano_project/src/home page/ai.js
@@ -0,0 +1,39 @@
+//code from the git hub free apis
+
+import ModelClient, { isUnexpected } from "@azure-rest/ai-inference";
+import { AzureKeyCredential } from "@azure/core-auth";
+
+const token = process.env["GITHUB_TOKEN"];
+const endpoint = "https://models.github.ai/inference";
+const model = "openai/gpt-4.1";
+
+export async function main() {
+
+  const client = ModelClient(
+    endpoint,
+    new AzureKeyCredential(token),
+  );
+
+  const response = await client.path("/chat/completions").post({
+    body: {
+      messages: [
+        { role:"system", content: "" },
+        { role:"user", content: "What is the capital of France?" }
+      ],
+      temperature: 1,
+      top_p: 1,
+      model: model
+    }
+  });
+
+  if (isUnexpected(response)) {
+    throw response.body.error;
+  }
+
+  console.log(response.body.choices[0].message.content);
+}
+
+main().catch((err) => {
+  console.error("The sample encountered an error:", err);
+});
+
